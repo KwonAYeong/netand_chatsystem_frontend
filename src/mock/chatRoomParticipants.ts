24bb0052bfe1b1f0cb18471b7519e416cb7bacfa
신재윤 <sin4691@naver.com>
2025-06-04 14:35:13 +0900

    feat:mock 데이터 추가

---
+++ src/mock/chatRoomParticipants.ts
@@ -0,0 +1,26 @@
+export const mockChatRoomParticipants = [
+  {
+    user_id: 1,
+    chat_room_id: 101,
+    joined_at: '2025-06-01T08:55:00Z',
+    last_read_message_id: 1,
+  },
+  {
+    user_id: 2,
+    chat_room_id: 101,
+    joined_at: '2025-06-01T08:56:00Z',
+    last_read_message_id: 1,
+  },
+  {
+    user_id: 1,
+    chat_room_id: 102,
+    joined_at: '2025-06-01T09:25:00Z',
+    last_read_message_id: 2,
+  },
+  {
+    user_id: 2,
+    chat_room_id: 102,
+    joined_at: '2025-06-01T09:26:00Z',
+    last_read_message_id: 2,
+  },
+];
