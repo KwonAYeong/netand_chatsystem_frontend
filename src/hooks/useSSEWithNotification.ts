c7f80b8dea6c162899b5f01f0e4a0a70c163f53b
KwonAYeong <129161150+KwonAYeong@users.noreply.github.com>
2025-06-30 22:27:24 +0900

    chore: 배포 주소로 변경

--- src/hooks/useSSEWithNotification.ts
+++ src/hooks/useSSEWithNotification.ts
@@ -51,8 +51,8 @@ export const useSSEWithNotification = (
       console.log('⚠️ 기존 EventSource 닫기');
       eventSourceRef.current.close();
     }
-    const eventSource = new EventSource(`http://localhost:8080/api/notification/subscribe?userId=${userId}`);
-    //const eventSource = new EventSource(`http://3.39.8.219:8080/api/notification/subscribe?userId=${userId}`);
+    // const eventSource = new EventSource(`http://localhost:8080/api/notification/subscribe?userId=${userId}`);
+    const eventSource = new EventSource(`http://3.39.8.219:8080/api/notification/subscribe?userId=${userId}`);
     eventSourceRef.current = eventSource;
 
     console.log('📢 SSE 연결 시작');
